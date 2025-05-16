bf480dcf781f141ab22ee5f63f9d01193897fac3
Martin <martintabordaaristizabal@gmail.com>
2025-05-16 17:42:23 -0500

    Se crea el main y e gameWindow

--- src/autonoma/lluviahamburguesa/main/Main.java
+++ src/autonoma/lluviahamburguesa/main/Main.java
@@ -4,10 +4,27 @@
  */
 package autonoma.lluviahamburguesa.main;
 
+import autonoma.atrapacomida.models.FoodField;
+import autonoma.atrapacomida.models.Player;
+import autonoma.atrapacomida.models.RefreshProgram;
+import autonoma.atrapacomida.views.GameWindow;
 /**
- *
+ *La clase main, crea y muestra la ventana.
  * @author marti
  */
 public class Main {
-    
+    public static void main(String[] args) {
+        Player player = new Player(0, 0, 50, 50);
+        FoodField foodField = new FoodField(0, 0, 500, 500);
+        foodField.setPlayer(player);
+        GameWindow window = new GameWindow();
+        window.setFoodField(foodField);
+        foodField.setGraphicContainer(window);
+        window.setSize(500, 500);
+        window.setTitle("Come  o muere ");
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+        RefreshProgram gameLoop = new RefreshProgram(foodField, window);
+        gameLoop.start();
+    }
 }
